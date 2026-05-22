5cd797e208d734b118037b81a8a0457f30937105
Nimrah S <nimrahs@Nimrahs-MacBook-Pro.local>
2026-05-22 16:33:58 -0700

    backend for login

--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,24 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import auth from "./routes/auth.js";
 
 dotenv.config();
 connectDB();
 
-
 const app = express();
-
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 app.use(express.json());
+app.use("/api/auth", auth);
 
 app.get("/", (req, res) => {
   res.send("Pick The Fit API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,36 +1,46 @@
 export default function Signup() {
-    async function signup(event) {
-        event.preventDefault();
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-    
-        await fetch(
-          'http://localhost:8080/signup',
-          {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-    
-            body: JSON.stringify({
-              username: username,
-              password: password
-            })
-          }
-        );
-    
-        window.location.href = '/';
-    
+  async function signup(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+      const res = await fetch('http://localhost:5050/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username,
+          password
+        })
+      });
+
+      const data = await res.json();
+
+      if (data.success) {
+        alert("Signup successful!");
+        window.location.href = "/";
+      } else {
+        alert(data.message || "Signup failed");
       }
-    
-      return (
-        <div className = "login_box">
-          <h1>Signup</h1>
-          <form onSubmit={signup}>
-            <input id="username"   placeholder="Username"/>
-            <input  id="password"  type="password"   placeholder="Password" />
-            <button>  Signup  </button>
-          </form>
-        </div>
-    );
+
+    } catch (err) {
+      console.log("ERROR:", err);
+      alert("Server not reachable (check backend)");
+    }
+  }
+
+  return (
+    <div className="login_box">
+      <h1>Signup</h1>
+
+      <form onSubmit={signup}>
+        <input id="username" placeholder="Username" />
+        <input id="password" type="password" placeholder="Password" />
+        <button>Signup</button>
+      </form>
+    </div>
+  );
 }
